1a4e22db0ac218437c09688445ccc69e9529af2c
Kun <asd1234567896138@gmail.com>
2024-04-14 16:30:13 +0800

    feat: 新增room頁面、連接FIREBASE.
    調整項目:
    1.新增vite環境變數設置
    2.新增room頁面
    3. style.css樣式
    4. firebase.js 設置firebase連接

---
+++ scripts/firebase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider,getAuth ,onAuthStateChanged,signInWithPopup} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+console.log(firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+export function login(){
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+        if(user){
+            // 已經登入
+            console.log('已經登入');
+            }else{
+            // 未登入
+            signInWithPopup(auth, provider);
+        }
+    })
+ 
+}
