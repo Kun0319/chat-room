e7c9b409fd8799e07c7b34d7dc87eaed589e2d3d
Kun <asd1234567896138@gmail.com>
2024-05-13 14:47:35 +0800

    feat:  新增登入功能、開啟聊天室邏輯

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-import { login } from "./scripts/firebase";
+import {createRoom, requireAuth} from "./scripts/firebase";
 import "./style.css";
 
-login();
+function setupEventListener() {
+    const createRoomBtn = document.getElementById("create-room-btn");
+    createRoomBtn.addEventListener("click", async() => {
+     const roomId= await createRoom();
+     window.location.href = `room.html?roomId=${roomId}`
+    });
+}
+
+requireAuth().then(setupEventListener)
--- scripts/firebase.js
+++ scripts/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider,getAuth ,onAuthStateChanged,signInWithPopup} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import {getFirestore,addDoc,collection} from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -19,16 +19,28 @@ console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
-export function login(){
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
+export function requireAuth(){
+    return new Promise((resolve, reject) => {
+        const auth = getAuth();
+        const unsubscribe  = onAuthStateChanged(auth, (user) => {
         if(user){
             // 已經登入
             console.log('已經登入');
+            resolve(user);
+            unsubscribe();
             }else{
             // 未登入
             signInWithPopup(auth, provider);
         }
     })
- 
+    })
+}
+
+export async function createRoom(){
+    const db = getFirestore();
+    const room = await addDoc(collection(db, "room"), {
+        name:"Chat room",
+        createdAt: new Date(),
+    })
+    return room.id;
 }
